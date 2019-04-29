5363c060e473e434bf8d44a3c27096ed7cca86ef
renant <renan_replay@live.com>
2019-04-29 20:15:15 -0300

    usando require-dir

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ const express = require('express')
 
 const routes = express.Router()
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const authMiddleware = require('./app/middlewares/auth')
 
-routes.post('/users', UserController.store)
-routes.post('/session', SessionController.store)
+const controllers = require('./app/controllers')
+
+routes.post('/users', controllers.UserController.store)
+routes.post('/session', controllers.SessionController.store)
+
+routes.get('/teste', authMiddleware, (req, res) => {
+  res.json({ ok: true })
+})
 
 module.exports = routes
